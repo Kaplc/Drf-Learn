8db6a7604f150940c533d7a08a23ec27cec23a44
Kapl <755536175@qq.com>
2023-01-07 19:05:57 +0800

    APIView的使用

--- drf_learn/drf_learn/apps/books/urls.py
+++ drf_learn/drf_learn/apps/books/urls.py
@@ -1,9 +1,15 @@
 from django.urls import re_path
-from .serializer_view import *
+from . import serializer_view
+from . import api_view
 
 urlpatterns = [
     # 多对象
-    re_path(r'books$', Books.as_view()),
+    re_path(r'books$', serializer_view.Books.as_view()),
     # 单对象
-    re_path(r'book/(?P<pk>\d)$', Book.as_view())
+    re_path(r'book/(?P<pk>\d)$', serializer_view.Book.as_view()),
+
+    # APIView多对象
+    re_path(r'apibooks$', api_view.Books.as_view()),
+    # APIView单对象
+    re_path(r'apibook/(?P<pk>\d)$', api_view.Book.as_view())
 ]
