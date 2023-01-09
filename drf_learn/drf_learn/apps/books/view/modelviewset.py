b3341f7a25248a993158af2c7638a1ed143eac20
Kapl <755536175@qq.com>
2023-01-09 18:08:38 +0800

    self.action的使用

--- drf_learn/drf_learn/apps/books/view/modelviewset.py
+++ drf_learn/drf_learn/apps/books/view/modelviewset.py
@@ -1,6 +1,6 @@
 """
 5个mixin和genericview的集合
-
+self.action当前的方法
 """
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
@@ -13,8 +13,16 @@ class Books(ModelViewSet):
     queryset = books = BookInfo.objects.all()  # 先定义查询集
     serializer_class = BookInfoSerializer  # 定义序列化器
 
+    def get_serializer_class(self):
+        """不同方法用不同的序列化器"""
+        if self.action == 'test':  # self.action当前的方法
+            return BookInfoSerializer
+        else:
+            return BookInfoSerializer
+
     @action(methods=['get'], detail=True)  # 自定义方法使用自动生成路由, detail==True--生成pk匹配路由
     def test(self, request, pk):
+        ser = self.get_serializer_class()
         pass
 
 
